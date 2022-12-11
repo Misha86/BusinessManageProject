8c5bbd5f0d5e2a371e906383037fc5f5dba3a1ac
Mykhailo Polishchuk <mpolishchuk86@gmail.com>
2022-12-12 01:36:08 +0200

    Added specialists to the main page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,55 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { Grid, Container, Card, CardActionArea, CardMedia, CardContent, Typography, Box } from '@mui/material';
+import UserService from '../services/user.service';
 
 const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    );
+  const [specialists, setSpecialists] = useState([]);
+
+  useEffect(() => {
+    const getSpecialists = async () => {
+      await UserService.getSpecialists()
+        .then((response) => {
+          console.log(response.data.results);
+          setSpecialists(response.data.results);
+        })
+        .catch((error) => {
+          console.log(error.data);
+        });
+    };
+    getSpecialists();
+  }, []);
+
+  return (
+    <Container maxWidth="xl">
+      <Grid container rowSpacing={1} mt={2} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        {specialists.length ? (
+          specialists.map((specialist) => (
+            <Grid key={specialist.email} item xs={4} sm={3} md={2}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia component="img" height="auto" image={specialist.avatar} alt={specialist.email} />
+                  <CardContent>
+                    <Typography gutterBottom variant="subtitle1" component="div">
+                      {`${specialist.first_name} ${specialist.last_name}`}
+                    </Typography>
+                    <Box component="p" sx={{ fontWeight: 900 }}>
+                      {specialist.position}
+                    </Box>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        ) : (
+          <Grid item xs display="flex" justifyContent="center" alignItems="center">
+            <Typography gutterBottom variant="h5" component="h5">
+              No Specialists yet!!!
+            </Typography>
+          </Grid>
+        )}
+      </Grid>
+    </Container>
+  );
 };
 
 export default Home;
--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,10 +1,22 @@
+import axios from 'axios';
+
 export default class UserService {
+  static api = axios.create({
+    baseURL: 'http://127.0.0.1:8000/api/',
+  });
+
   static getUser() {
     const auth = JSON.parse(localStorage.getItem('auth'));
     return auth?.user;
   }
+
   static getUserGroups() {
     const auth = JSON.parse(localStorage.getItem('auth'));
     return auth?.user.groups;
+  }
+
+  static async getSpecialists() {
+    const response = await this.api.get('/specialists/');
+    return response;
   }
 }
